becd92aae58cc452210e0886891ec1fc703d09af
VovaZaycev <vovazaycev0306@gmail.com>
2021-10-27 20:08:43 +0300

    delete and update

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -36,7 +36,16 @@
                             </td>
 
                             <td>
-                                Удалить{{$task->id}}
+                                <form method="post" action="{{ route('tasks.delete', $task->id)}}">
+                                    {{csrf_field()}}
+                                    {{method_field('DELETE')}}
+                                    <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                </form>
+                                <form method="post" action="{{ route('tasks.edit', $task->id)}}">
+                                    {{csrf_field()}}
+                                    {{method_field('GET')}}
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
